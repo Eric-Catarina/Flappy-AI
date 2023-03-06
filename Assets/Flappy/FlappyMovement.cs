db5dcb26ad52f5c1020862e8ba7ffed5c59571ed
Eric-Catarina <orment.cp@gmail.com>
2023-03-05 22:23:26 -0300

    first commit

---
+++ Assets/Flappy/FlappyMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    [SerializeField] private float forcaDoSoco = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * forcaDoSoco, ForceMode.Impulse);
+        }
+    }
+}
